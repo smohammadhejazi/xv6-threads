bccc01d284d92ae33670223b05be22dd0ad0da17
smohammadhejazi <78293534+smohammadhejazi@users.noreply.github.com>
2021-07-18 10:19:42 -0700

    Add a new test

--- xv6/test.c
+++ xv6/test.c
@@ -5,29 +5,29 @@
 
 //1
 
-int x = 0;
+// int x = 0;
 
-void function(void *arg)
-{
-    for(int i = 0; i < 5; i++) 
-    {
-      x++;
-      printf(1, "Thread x = %d\n", x);  
-      sleep(100);
-    }
-    exit();
-}
+// void function(void *arg)
+// {
+//     for(int i = 0; i < 5; i++) 
+//     {
+//       x++;
+//       printf(1, "Thread x = %d\n", x);  
+//       sleep(100);
+//     }
+//     exit();
+// }
 
-int main(int argc, char *argv[]) 
-{
-    int tid;
-    int arg = 1;
-    tid = thread_create(&function, &arg);
-    thread_join(tid);
-    printf(1, "Parent x = %d\n", x);
-    printf(1, "Parent terminated.\n");
-    exit();
-}
+// int main(int argc, char *argv[]) 
+// {
+//     int tid;
+//     int arg = 1;
+//     tid = thread_create(&function, &arg);
+//     thread_join(tid);
+//     printf(1, "Parent x = %d\n", x);
+//     printf(1, "Parent terminated.\n");
+//     exit();
+// }
 
 // -1
 
@@ -105,45 +105,44 @@ int main(int argc, char *argv[])
 
 // 4
 
-// int main(int argc, char *argv[]) 
-// {
-//     int mat[40][40];
-//     for (int i = 0; i < 40; i++)
-//     {
-//       for (int j = 0; j < 40; j++)
-//       {
-//         mat[i][j] = i;
-//       }
-//     }
-//     printf(1, "%d\n", mat[39][39]);
-//     exit();
-// }
+int arr[20];
+
+void function(void *arg)
+{
+  int num = *((int *)arg);
+  for(int i = 0; i < 5; i++)
+  {
+    arr[num + i] =  num;
+  }
+
+  exit();
+}
+
+int main(int argc, char *argv[]) 
+{
+  int tid[4];
+  
+  int arg0 = 0;
+  int arg1 = 5;
+  int arg2 = 10;
+  int arg3 = 15;
+
+  tid[0] = thread_create(&function, &arg0);
+  tid[1] = thread_create(&function, &arg1);
+  tid[2] = thread_create(&function, &arg2);
+  tid[3] = thread_create(&function, &arg3);
+
+  for(int i = 0; i < 4; i++)
+  {
+    thread_join(tid[i]);
+  }
+  
+  for (int i = 0; i < 20; i++)
+  {
+    printf(1, "[%d] = %d\n", i, arr[i]);
+  }
+
+  exit();
+}
 
 // -4
-
-// 5
-
-// void function(void *arg)
-// {
-//     int mat[20000][20000];
-//     for (int i = 0; i < 20000; i++)
-//     {
-//       for (int j = 0; j < 20000; j++)
-//       {
-//         mat[i][j] = i;
-//       }
-//     }
-//     printf(1, "Thread: %d\n", mat[10000][10000]);
-//     exit();
-// }
-
-// int main(int argc, char *argv[]) 
-// {
-//     int tid;
-//     tid = thread_create(&function, 0);
-//     thread_join(tid);
-//     printf(1, "Parent terminated.\n");
-//     exit();
-// }
-
-// -5
